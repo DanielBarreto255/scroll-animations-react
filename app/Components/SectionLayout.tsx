62f8e3ce3c3e38abb5eb5367dcf39ef5a9c23eb0
Daniel Barreto <danyel.rbarreto@gmail.com>
2024-05-24 11:59:25 +0100

    fourth commit - scroll and body images done

---
+++ app/Components/SectionLayout.tsx
@@ -0,0 +1,20 @@
+import react from "react";
+
+interface SectionLayoutProps {
+  children: React.ReactNode;
+  ref?: React.RefObject<HTMLDivElement>;
+}
+
+function SectionLayout({ children, ref }: SectionLayoutProps) {
+  return (
+    <div
+      style={{
+        padding: "5rem 10rem",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default SectionLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,53 @@
 "use client";
 import React from "react";
 import Header from "./Components/Header/Header";
-
+import styled from "styled-components";
+import SectionLayout from "./Components/SectionLayout";
+import Card from "./Components/Card";
+import { cards } from "./utils/cards";
 export default function Home() {
   return (
     <>
       <Header />
+      <MainStyled>
+        <SectionLayout>
+          <div className="cards">
+            {cards.map((card, index) => {
+              return (
+                <Card
+                  key={index}
+                  title={card.title}
+                  description={card.description}
+                  image={card.image}
+                />
+              );
+            })}
+          </div>
+        </SectionLayout>
+      </MainStyled>
     </>
   );
 }
+
+const MainStyled = styled.main`
+  min-height: 100vh;
+  width: 100%;
+
+  .cards {
+    position: absolute;
+    display: grid;
+    grid-template-columns: repeat(5, 30rem);
+    gap: 4rem;
+  }
+
+  .video {
+    padding: 2rem;
+    background-color: #161616;
+    border-radius: 1rem;
+    iframe {
+      border: none;
+      width: 100%;
+      height: 52rem;
+    }
+  }
+`;
